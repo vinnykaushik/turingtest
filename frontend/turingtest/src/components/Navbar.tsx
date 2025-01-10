a0560cdc33ece46107482baf799a190d96adf9be
vinnykaushik <148011913+vinnykaushik@users.noreply.github.com>
2025-01-10 13:59:32 -0500

    Tune navbar look, change <a> to <Link/>

--- frontend/turingtest/src/components/Navbar.tsx
+++ frontend/turingtest/src/components/Navbar.tsx
@@ -1,19 +1,23 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-background font-mono">
-      <div className="text-accent text-2xl">
-        turing test
+      <div className="text-accent text-2xl w-fit h-fit p-1 rounded-lg bg-background hover:brightness-90 hover:transition-all ">
+        <Link href={"/"}>
+          turing test
+        </Link>
       </div>
       <div className="flex space-x-6">
-        <a href="#about" className="text-foreground hover:underline">
+        <Link href="about" className="text-foreground hover:underline">
           about
-        </a>
-        <a href="#how-to" className="text-foreground hover:underline">
+        </Link>
+        <Link href="#how-to" className="text-foreground hover:underline">
           how to
-        </a>
-        <a href="https://github.com/vinnykaushik/turingtest" className="text-foreground hover:underline">
+        </Link>
+        <Link href="https://github.com/vinnykaushik/turingtest" target="_blank" className="text-foreground hover:underline">
           github
-        </a>
+        </Link>
       </div>
     </nav>
   );
